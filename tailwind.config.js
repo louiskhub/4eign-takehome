a4758cf273bc6ec5b98d35600a54b9cb1c532c3f
Louis Kapp <l.kapp@4eign.de>
2024-07-30 22:55:26 +0200

    add dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+// @ts-check
+import { join } from 'path';
+
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton({ themes: { preset: ['skeleton'] } })
+	]
+};
